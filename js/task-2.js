538bd27e99884affd30e97606c62bb969539a7c9
TaratutaDmytro <dtaratuta13@gmail.com>
2026-08-15 16:03:26 +0200

    Refactor storage logic and implement gallery display
    Removed Storage class and related methods. Added functionality to display gallery items using HTML.

--- js/task-2.js
+++ js/task-2.js
@@ -1,35 +1,11 @@
-class Storage {
-  #items;
+const gallery = document.querySelector(".gallery");
 
-  constructor(items) {
-    this.#items = items;
-  }
+const galleryItems = images.map(({ url, alt }) => {
+  return `
+    <li class="gallery-item">
+      <img src="${url}" alt="${alt}" class="gallery-image">
+    </li>
+  `;
+}).join("");
 
-  getItems() {
-    return this.#items;
-  }
-
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-
-  removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-
-    if (index !== -1) {
-      this.#items.splice(index, 1);
-    }
-  }
-}
-
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-storage.removeItem("Scaner");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+gallery.insertAdjacentHTML("beforeend", galleryItems);
